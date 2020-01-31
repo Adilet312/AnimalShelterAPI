b92b0f07d60cb3f119c3300ce23d80ce92b03bb4
Adilet312 <adiletm@hotmail.com>
2020-01-31 13:40:27 -0800

    Add Animal and AnimalContext files in Modules and Table that was created by migrating.

---
+++ AnimalShelter/Models/AnimalRent.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+using System;
+namespace AnimalShelter
+{
+    public enum AnimalType {Dog,Cat};
+    public class AnimalRent
+    {
+        public string AnimalName {get;set;}
+        public  AnimalType AnimalType {get;set;}
+        public DateTime DateOfArrived {get;set;}
+        public Boolean AnimalAvailability {get;set;}
+        
+    }
+}
